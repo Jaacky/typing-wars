847ebcb3d1b3cfe60426a4ad75f372a15f8c3df8
Jacky <jaackyvan@gmail.com>
2018-10-09 22:47:17 -0400

    Refactor - map, removing old files

--- state/map.go
+++ state/map.go
@@ -1,5 +1,10 @@
 package state
 
+import (
+	"github.com/Jaacky/typing-wars/communication"
+	"github.com/Jaacky/typing-wars/types"
+)
+
 // Map struct
 type Map struct {
 	Bases map[string]*Base
@@ -7,9 +12,25 @@ type Map struct {
 }
 
 // NewMap Initialization
-func (m *Map) NewMap() *Map {
+func (m *Map) NewMap(clients *[]communication.Client) *Map {
+	bases := make(map[string]*Base)
+
+	// Hard coding 2 players only
+	for i := 0; i < len(clients); i++ {
+		client := clients[i]
+		var point *types.Point
+		if i == 0 {
+			point = types.NewPoint(5, 50)
+		} else {
+			point = types.NewPoint(95, 50)
+		}
+
+		base := NewBase(client.ID, point)
+		bases = append(bases, base)
+	}
+
 	return &Map{
-		Bases: make(map[string]*Base),
+		Bases: bases,
 		Units: make(map[string]*Unit),
 	}
 }
