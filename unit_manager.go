8c384990cab651f7ca38993b270e2c0331c2646c
Jacky <jaackyvan@gmail.com>
2018-11-07 23:00:13 -0500

    Stopping unit after target reached and handle unit add

--- unit_manager.go
+++ unit_manager.go
@@ -20,9 +20,26 @@ func (um *UnitManager) updateUnits() {
 	log.Println("Updating units")
 	for _, units := range um.space.Units {
 		for _, unit := range *units {
-			log.Println("Unit update", unit.Position)
-			velocity := types.NewVector(2, 0)
-			unit.Position = unit.Position.Add(velocity)
+			log.Printf("Unit <%s> belonging to %s updated position: %v", unit.Word, unit.Owner, unit.Position)
+			if unit.Position.Equal(unit.Target.Position) {
+				continue
+			} else {
+				// TODO: Calculate vector to add to unit based off of unit's position and target's position
+				// This will do for now as units are only going straight across horizontally
+				velocity := types.NewVector(unit.Speed, 0)
+				unit.Position = unit.Position.Add(velocity)
+			}
 		}
+	}
+}
+
+func (um *UnitManager) addUnit(unit *Unit) {
+	userUnits := *um.space.Units[unit.Owner]
+	if _, ok := userUnits[unit.Word]; !ok {
+		userUnits[unit.Word] = unit
+		log.Printf("Adding unit <%s> for client %s", unit.Word, unit.Owner)
+		log.Printf("All units now: %v", um.space.Units)
+	} else {
+		log.Printf("Unit with word already exists")
 	}
 }
