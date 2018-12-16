99fcaab6e5e72a52628af714c4b60f9425cbbe5b
Jacky <jaackyvan@gmail.com>
2018-12-16 11:23:42 -0800

    Getting port from env

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	} else {
+		port = ":" + port
+	}
+
 	server := NewServer()
 	server.Listen()
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Listening on port - ", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 	log.Println("After listen and serve")
 }
