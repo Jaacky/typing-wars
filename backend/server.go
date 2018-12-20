4dba71e1ac9df75a93f139e3b755e58736b6006f
Jacky <jaackyvan@gmail.com>
2018-12-19 23:46:24 -0500

    Serving UI from go server

--- backend/server.go
+++ backend/server.go
@@ -77,8 +77,10 @@ func (server *Server) Listen() {
 		client.Listen()
 	}
 
+	http.Handle("/", http.FileServer(http.Dir("./ui/dist")))
 	http.HandleFunc("/connect", connect)
-	http.HandleFunc("/", home)
+
+	// http.HandleFunc("/", home)
 
 	go server.manageRooms()
 	log.Println("End of serverlisten")
